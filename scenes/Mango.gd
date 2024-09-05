b9e15eddd2dc0b3da029626fecb40f3ad6cc3cd0
DaqCode <dlim0012.gaming@gmail.com>
2024-09-05 08:16:16 -0700

    Redid Textures for Button

--- scenes/Mango.gd
+++ scenes/Mango.gd
@@ -1,7 +1,7 @@
 extends Sprite2D
 
 func _on_mango_area_mouse_entered() -> void:
-	# mango.scale = Vector2(0.09, 0.106)
+	# mango.scale = Vector2(1.2, 1.2)
 	# if Input.is_action_just_pressed("leftMouse"):
 	# 	var timer = get_tree().create_timer(2.0)
 	# 	timer.connect("timeout", Callable(self, "_on_mango_timer_timeout"))
@@ -11,5 +11,7 @@ func _on_mango_timer_timeout() -> void:
 	print("mango")
 	get_tree().quit()
 
-func _on_mango_area_mouse_exited() -> void:
-	mango.scale = Vector2(0.08, 0.094)
+	#mango.scale = Vector2(1, 1)
+
+func _on_mango_area_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	pass # Replace with function body.
